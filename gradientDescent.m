5ca9ee7e9f1f8ddb8bcfb97d4e2e735ee3d82bf9
Prarabdhaa <43156257+Prarabdhaa@users.noreply.github.com>
2018-10-05 11:23:24 -0400

    Update gradientDescent.m

--- gradientDescent.m
+++ gradientDescent.m
@@ -6,10 +6,12 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-%theta_len = length(theta);
+
 
 for iter = 1:num_iters
 
+% theta_len = length(theta);
+  
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
@@ -17,25 +19,29 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    
-	temp_theta = theta;
-	for j = 1:theta_len		
-		value = 0;
 
-		for i = 1:m
-			value += (X(i,:) * theta- y(i,:)) * X(i,j);
-		end
-
-		temp_theta(j,:) = temp_theta(j,:) - ((alpha/m)*value);
-	end
-
-	theta = temp_theta;
+	theta = theta-(alpha/length(y)) * (X' * (X*theta - y));
+% alternatively :
+% 	temp_theta = theta;
+% 	for j = 1:theta_len		
+% 		value = 0;
+% 
+% 		for i = 1:m
+% 			value = value + (X(i,:) * theta- y(i,:)) * X(i,j);
+% 		end
+% 
+% 		temp_theta(j,:) = temp_theta(j,:) - ((alpha/m)*value);
+% 	end
+% 
+% 	theta = temp_theta;
 
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);    
+    
 
 end
 
+end
 end
